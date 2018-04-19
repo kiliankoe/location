a829eb2b8cafb6dd5d2dd04a8efc94491129515c
Kilian Koeltzsch <me@kilian.io>
2018-04-19 18:00:51 +0200

    bump version to 0.2.1

--- Formula/location.rb
+++ Formula/location.rb
@@ -1,8 +1,8 @@
 class Location < Formula
     desc "cli to get your device's physical location and output it as json."
     homepage "https://github.com/kiliankoe/location"
-    url "https://github.com/kiliankoe/location/archive/0.2.0.tar.gz"
-    sha256 "209eaa85ef9da3e465f20b8f5a9ca1a63b3f9ac34cd43896c2f22967195ea612"
+    url "https://github.com/kiliankoe/location/archive/0.2.1.tar.gz"
+    sha256 "562eb522f133a578630a359c0f2f83ed51e243c7a090f96cea7e3db7961f100a"
     head "https://github.com/kiliankoe/location.git"
 
     depends_on :xcode
