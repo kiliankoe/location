4480e106e011862ca583f49dfc0a4fd417f8126e
Florian Schmidt <florian.schmidt.1994@icloud.com>
2018-06-25 14:09:03 +0200

    Rename to `whereami`
    Closes #2

--- Formula/location.rb
+++ Formula/location.rb
@@ -1,15 +1,15 @@
-class Location < Formula
+class Whereami < Formula
     desc "cli to get your device's physical location and output it as json."
-    homepage "https://github.com/kiliankoe/location"
-    url "https://github.com/kiliankoe/location/archive/0.3.0.tar.gz"
+    homepage "https://github.com/kiliankoe/whereami"
+    url "https://github.com/kiliankoe/whereami/archive/0.3.0.tar.gz"
     sha256 "86837503898d470a4e47b65116c4400e77f44dac58413cdcf352edea9df12810"
-    head "https://github.com/kiliankoe/location.git"
+    head "https://github.com/kiliankoe/whereami.git"
 
     depends_on :xcode
 
     def install
-        build_path = "#{buildpath}/.build/release/location"
-        ohai "Building location"
+        build_path = "#{buildpath}/.build/release/whereami"
+        ohai "Building whereami"
         system("swift build --disable-sandbox -c release --static-swift-stdlib")
         bin.install build_path
     end
